d5dcbec3ca2745738464001f1cde796d66d07ba3
Mor1sMori <morsmis2@gmail.com>
2026-09-13 14:42:44 +0400

    Сделана 1 Лаба

---
+++ 1/main.go
@@ -0,0 +1,99 @@
+/*Лаба 1. Задание 1.
+
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	fmt.Println(time.Now())
+}
+*/
+
+/* Задание 2.
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	var a int = 64
+	var b float64 = 5.5
+	var c string = "blaBlabla"
+	var d bool = true
+	fmt.Println(a, b, c, d)
+}
+*/
+
+// Задание 3.
+/*
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	a := 64
+	b := 5.5
+	c := "blaBlabla"
+	d := true
+	fmt.Println(a, b, c, d)
+}
+*/
+
+// Задание 4.
+/*
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	a := 6
+	b := 2
+	c := a + b
+	d := a - b
+	e := a * b
+	f := a / b
+	g := a % b
+	fmt.Println(c, d, e, f, g)
+}
+*/
+
+// Задание 5.
+/*
+package main
+
+import "fmt"
+
+func sumdiff(a float64, b float64) (float64, float64) {
+	sum := a + b
+	diff := a - b
+	return sum, diff
+}
+func main() {
+	sum, diff := sumdiff(10.1, 1.5)
+	fmt.Println(sum, diff)
+}
+*/
+
+// Задание 6.
+/*
+package main
+
+import "fmt"
+
+func avgg(a float64, b float64, c float64) float64 {
+	avg := (a + b + c) / 3
+	return avg
+}
+func main() {
+	avg := avgg(3, 4, 6)
+	fmt.Println(avg)
+}
+*/
