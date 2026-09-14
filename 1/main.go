31a7d81fe8b98faa45c50323d94277f97c098d66
Mor1sMori <morsmis2@gmail.com>
2026-09-14 20:47:46 +0400

    Передел 1 лабораторной и начало 2

--- 1/main.go
+++ 1/main.go
@@ -1,5 +1,3 @@
-/*Лаба 1. Задание 1.
-
 package main
 
 import (
@@ -7,93 +5,47 @@ import (
 	"time"
 )
 
-func main() {
-	fmt.Println(time.Now())
-}
-*/
-
-/* Задание 2.
-package main
-
-import (
-	"fmt"
-)
-
-func main() {
-	var a int = 64
-	var b float64 = 5.5
-	var c string = "blaBlabla"
-	var d bool = true
-	fmt.Println(a, b, c, d)
-}
-*/
-
-// Задание 3.
-/*
-package main
-
-import (
-	"fmt"
-)
-
-func main() {
-	a := 64
-	b := 5.5
-	c := "blaBlabla"
-	d := true
-	fmt.Println(a, b, c, d)
-}
-*/
-
-// Задание 4.
-/*
-package main
-
-import (
-	"fmt"
-)
-
-func main() {
-	a := 6
-	b := 2
-	c := a + b
-	d := a - b
-	e := a * b
-	f := a / b
-	g := a % b
-	fmt.Println(c, d, e, f, g)
-}
-*/
-
 // Задание 5.
-/*
-package main
-
-import "fmt"
 
 func sumdiff(a float64, b float64) (float64, float64) {
 	sum := a + b
 	diff := a - b
 	return sum, diff
 }
-func main() {
-	sum, diff := sumdiff(10.1, 1.5)
-	fmt.Println(sum, diff)
-}
-*/
 
 // Задание 6.
-/*
-package main
-
-import "fmt"
-
 func avgg(a float64, b float64, c float64) float64 {
 	avg := (a + b + c) / 3
 	return avg
 }
+
 func main() {
+	fmt.Println("Задание 1:", time.Now())
+
+	var a int = 64
+	var b float64 = 5.5
+	var c string = "blaBlabla"
+	var d bool = true
+	fmt.Println("Задание 2:", a, b, c, d)
+
+	a1 := 64
+	b2 := 5.5
+	c3 := "blaBlabla"
+	d4 := true
+	fmt.Println("Задание 3:", a1, b2, c3, d4)
+
+	a11 := 6
+	b11 := 2
+	c11 := a11 + b11
+	d11 := a11 - b11
+	e := a11 * b11
+	f := a11 / b11
+	g := a11 % b11
+	fmt.Println("Задание 4:", c11, d11, e, f, g)
+
+	sum, diff := sumdiff(10.1, 1.5)
+	fmt.Println("Задание 5:", sum, diff)
+
 	avg := avgg(3, 4, 6)
-	fmt.Println(avg)
+	fmt.Println("Задание 6:", avg)
 }
-*/
---
+++ 2/main2.go
@@ -0,0 +1,15 @@
+// Лаба 2. Задание 1.
+
+package main
+
+import "fmt"
+
+func main() {
+	var num int
+	fmt.Scanln(&num)
+	if num%2 == 0 {
+		fmt.Println("Число четное")
+	} else {
+		fmt.Println("Число не четное")
+	}
+}
